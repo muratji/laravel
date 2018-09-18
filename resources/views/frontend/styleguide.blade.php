f85869053aee5084c933fe9908de90e40929177f
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2018-09-18 12:05:22 +0100

    Updates styleguide styles

--- resources/views/frontend/styleguide.blade.php
+++ resources/views/frontend/styleguide.blade.php
@@ -38,6 +38,10 @@
 				'headings' => [
 					'Heading 1' => 'h1',
 					'Heading 2' => 'h2',
+					'Heading 3' => 'h3',
+					'Heading 4' => 'h4',
+					'Heading 5' => 'h5',
+					'Heading 6' => 'h6',
 				],
 			],
 		],
--- resources/views/styleguide/component.blade.php
+++ resources/views/styleguide/component.blade.php
@@ -3,7 +3,7 @@
 		<h3 class="s-h3">{{ $title }}</h3>
 
 		@if ($meta ?? false)
-			<dl class="s-push-top">
+			<dl class="s-push-top--small s-meta">
 				@foreach ($meta as $key => $value)
 					<dt>{{ $key }}</dt>
 					<dd>
@@ -26,7 +26,9 @@
 		@endif
 	</div>
 
-	<div class="s-grid__item s-component__preview">
-		{{ $slot }}
+	<div class="s-grid__item s-component__preview-wrapper">
+		<div class="s-component__preview">
+			{{ $slot }}
+		</div>
 	</div>
 </div>
