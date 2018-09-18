255ae9d28f6fe6a5d80e2abe307fbcb6048fc8de
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2018-09-18 09:44:38 +0100

    Simplify button example

--- resources/assets/js/components/common/magic-button.vue
+++ resources/assets/js/components/common/magic-button.vue
@@ -1,54 +1,27 @@
 <template>
-	<button class="button"
+	<button
+		class="button"
 		:class="classes"
 		:disabled="$props.disabled"
 		v-text="$props.cta" />
 </template>
 
 <script>
-	const classes = {
-		sizes: {
-			small: 'button--small',
-			medium: null,
-			large: 'button--large',
-		},
-		types: {
-			primary: null,
-			secondary: 'button--secondary',
-			positive: 'button--positive',
-			negative: 'button--negative',
-		},
-	};
-
 	export default {
 		props: {
-			cta: {
-				type: String,
-				required: true,
-			},
-			disabled: {
-				type: Boolean,
-			},
-			size: {
-				type: String,
-				default: 'medium',
-				validator: size => Object.keys(classes.sizes).includes(size),
-			},
-			type: {
-				type: String,
-				default: 'primary',
-				validator: type => Object.keys(classes.types).includes(type),
-			},
+			cta: String,
+			disabled: Boolean,
+			size: String,
+			type: String,
 		},
 
 		computed: {
 			classes() {
 				return {
-					[classes.sizes[this.$props.size]]: this.$props.size,
-					[classes.types[this.$props.type]]: this.$props.type,
+					[`button--${this.$props.size}`]: this.$props.size,
+					[`button--${this.$props.type}`]: this.$props.type,
 				};
 			},
 		},
 	};
-
 </script>
